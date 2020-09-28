988eb52067f6bca4dccd3d60477d6c55b4261077
Noel Jacob <noeljacob91@gmail.com>
2020-09-28 17:28:45 +0530

    Added demo pattern

--- node/app.js
+++ node/app.js
@@ -6,11 +6,11 @@ const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url,true).query;
-  if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
+  const queryObject = url.parse(req.url, true).query;
+  if (!queryObject['count']) {
+    res.end(`Please use command curl http://${hostname}:${port}/?count=Integer \n`);
   } else {
-    res.end(say(queryObject['name']) + '\n');
+    res.end(say(queryObject['count']) + '\n');
   }
 });
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,14 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+    println!("The Rust function say() received {}", s);
+    let mut r = String::from("");
+    let num = s.parse::<i64>().unwrap();
+    for x in 0..=num {
+        for _ in 0..=x {
+            r += &"1"
+        }
+        r += "\n"
+    }
+    return r;
 }
